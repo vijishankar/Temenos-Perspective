c51e45bf52ccee08f4ff6a1ac9d9309347b78040
vijishankar <67778907+vijishankar@users.noreply.github.com>
2021-04-28 21:31:14 +0530

    Update Deploy-ResourceGroup.ps1

--- Deploy-ResourceGroup.ps1
+++ Deploy-ResourceGroup.ps1
@@ -6,15 +6,25 @@ Function Deploy-ResourceGroup {
         [Int]$capacity = 2
     )
     begin {
-        
+        # $payload = $Env:payload
     }
     process {
+
+        $startTime = (Get-Date)
+
+        ##### Import custom modules ########
+        $scriptRoot = "$Env:SYSTEM_DEFAULTWORKINGDIRECTORY\$Env:RELEASE_PRIMARYACTIFACTSOURCEALIAS"
+        Write-Output ("Script root is {0}" -f $scriptRoot)
+        Get-Childitem –Path $scriptRoot -Include *.psm1 -Recurse | ForEach-Object {
+            Import-Module (Resolve-Path($_)) -Force
+            Write-Output ("Importing module {0}" -f $_)
         }
 
         ###### Getting Azure DevOps variables ########
- 
-       
-       
+
+        $templateFile = "resourceGroup.json"
+        $aksResourceGroup = "Temenos-AKS"
+        $location = "east-us"
         
 
         ########### Creating deployment parameter set ###########
@@ -25,7 +35,7 @@ Function Deploy-ResourceGroup {
         {
             Write-Output ("################################ Creating ResourceGroup ####################################")
             $Parameters = @{ 
-                templateFile = "resourceGroup.json"
+                templateFile = $templateFile
                 Verbose = $true
                 templateParameterObject = @{
                    $aksResourceGroup = "Temenos-AKS"
@@ -40,8 +50,8 @@ Function Deploy-ResourceGroup {
             Write-Output ("ResourceGroup already exists")
         }
 
-       
-        
+        $endTime = (Get-Date)
+        'Script Duration --> {0:mm} min {0:ss} sec' -f ($endTime-$startTime)
 
     }
 }
