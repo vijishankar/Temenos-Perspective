98437c31f9fc93cad03053da1e428145bc3b6999
vijishankar <67778907+vijishankar@users.noreply.github.com>
2021-04-29 01:24:08 +0530

    Update Deploy-ResourceGroup.ps1

--- Deploy-ResourceGroup.ps1
+++ Deploy-ResourceGroup.ps1
@@ -21,24 +21,17 @@ Function Deploy-ResourceGroup {
 
         ########### Creating deployment parameter set ###########
 
-        $resourceGroup = az group exists --name $aksResourceGroup
-
-        if (!$resourceGroup)
-        {
-            Write-Output ("################################ Creating ResourceGroup ####################################")
+        
             $Parameters = @{ 
                 templateParameterObject = @{
                    aksResourceGroup = 'Temenos-AKS'
                    location = 'east-us'
                 }
-            }
-            $result = az group deployment create @Parameters --template-file resourceGroup.json
-            Write-Output $result
+                }
+           az group deployment create @Parameters --template-file resourceGroup.json
+         
 
-        }
-        else {
-            Write-Output ("ResourceGroup already exists")
-        }
+        
 
       
 
