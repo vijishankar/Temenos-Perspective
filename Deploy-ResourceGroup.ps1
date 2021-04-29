af615728309e74fd0518b9fdb5a0e415bc4875b6
vijishankar <67778907+vijishankar@users.noreply.github.com>
2021-04-29 10:06:28 +0530

    Update Deploy-ResourceGroup.ps1

--- Deploy-ResourceGroup.ps1
+++ Deploy-ResourceGroup.ps1
@@ -1,18 +1,57 @@
 Function Test-ScriptBlock
 {
     Param
-    (
-        [string[]]$ParameterName
+    ( 
     )
     BEGIN
-    {
-        $Results = $ParameterName
-        Write-Host $Results
+    {    
     }
  
     PROCESS
     {
-        Write-Host "In Process block"
+        $startTime = (Get-Date)
+
+        ##### Import custom modules ########
+        $scriptRoot = "$Env:SYSTEM_DEFAULTWORKINGDIRECTORY\$Env:RELEASE_PRIMARYACTIFACTSOURCEALIAS"
+        Write-Output ("Script root is {0}" -f $scriptRoot)
+        Get-Childitem –Path $scriptRoot -Include *.psm1 -Recurse | ForEach-Object {
+            Import-Module (Resolve-Path($_)) -Force
+            Write-Output ("Importing module {0}" -f $_)
+        }
+
+        ###### Getting Azure DevOps variables ########
+
+        $templateFile = "resourceGroup.json"
+        $aksResourceGroup = "Temenos-AKS"
+        $location = "east-us"
+        
+
+        ########### Creating deployment parameter set ###########
+
+        $resourceGroup = Get-AzResourceGroup -Name $aksResourceGroup -ErrorAction SilentlyContinue
+
+        if (!$resourceGroup)
+        {
+            Write-Output ("################################ Creating ResourceGroup ####################################")
+            $Parameters = @{ 
+                templateFile = $templateFile
+                Verbose = $true
+                templateParameterObject = @{
+                   $aksResourceGroup = "Temenos-AKS"
+                   $location = "east-us"
+                }
+            }
+            $result = New-AzResourceGroupDeployment @Parameters
+            Write-Output $result
+
+        }
+        else {
+            Write-Output ("ResourceGroup already exists")
+        }
+
+        $endTime = (Get-Date)
+        'Script Duration --> {0:mm} min {0:ss} sec' -f ($endTime-$startTime)
+
     }
     END
     {
